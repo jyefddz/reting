8fecb0a38351f225b535f2d93b9416bc0af9885f
jyefddz <1065697146@qq.com>
2022-07-13 21:12:25 +0800

    初始化

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+      'postcss-pxtorem': {
+        rootValue: (module) => (/vant/gi.test(module.file) ? 37.5 : 75),
+  
+        //   适配的属性
+        propList: ['*']
+      }
+    }
+  }
+  
